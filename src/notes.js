1cf784deb2d25ea22680e6d1d4943bb4d15c04f2
Shubhangi Jindal <Shubhangi@softobiz.com>
2023-03-31 16:35:30 +0530

    complete

---
+++ src/notes.js
@@ -0,0 +1,34 @@
+const notes = [
+    {
+        key:1,
+        title:"Delegation",
+        content:" Q: How many programmers does it look to change a light bulb? A.none its a hardware problem"
+    },
+    {
+        key:2,
+        title:"Loops",
+        content:" Q: How many programmers does it look to change a light bulb?"
+    },
+    {
+        key:3,
+        title:"Arrays",
+        content:" Q: How many programmers does it look to change a light bulb?"
+    },
+    {
+        key:4,
+        title:"Hardware vs. Software",
+        content:" Q: How many programmers does it look to change a light bulb?"
+    },
+    {
+        key:5,
+        title:"New note",
+        content:" Q: How many programmers does it look to change a light bulb?"
+    },
+    {
+        key:6,
+        title:"New note",
+        content:" Q: How many programmers does it look to change a light bulb?"
+    }
+]
+
+export default notes;
